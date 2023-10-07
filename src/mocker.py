e11a3612f7f8ac308d3ee071f58d39bd64a10f70
xlzhang <xlzhang@xreal.com>
2023-10-07 14:30:11 +0800

    update
    rmbg

--- src/mocker.py
+++ src/mocker.py
@@ -9,4 +9,4 @@ model_shape = ShapE()
 def predict(request_id, prompt, image_url):
     input_image = f"{input_dir}/{request_id}_input.png"
     download_file(image_url, input_image)
-    return model_shape.predict(request_id, input_image, prompt)
+    return model_shape.predict(request_id, input_image, prompt, True)
